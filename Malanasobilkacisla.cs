58d3bbd807e3541e39e5b8a5c7673b8d8235c365
Ducvald <121226512+Ducvald@users.noreply.github.com>
2023-01-11 19:43:24 +0100

    Vypočítá malou násobilku jakéhokoliv čísla.

---
+++ Malanasobilkacisla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyklyCviceni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int b = 1;
+            int cislo;
+            Console.WriteLine("napiš libovolné číslo a já ti vypíšu jeho násobky do 10");
+            Console.WriteLine("----------------------------------------------------------");
+            cislo = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                i = b * cislo;
+                Console.WriteLine(i);
+                b++;
+            }
+            while (i < (cislo * 10));
+            Console.WriteLine("Celá malá násobilka" + " " + cislo);
+        }
+    }
+}
